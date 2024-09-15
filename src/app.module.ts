202959ce93f38c21ae9b3af76f30a76ac962943d
LaH198 <haiche198@gmail.com>
2024-09-15 13:45:25 +0700

    Upload migration with typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './user/entities/user.entity';
+import { dataSourceOptions } from 'db/data-source';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'blog_nestjs',
-      entities: [User],
-      synchronize: true,
-    }),
-  ],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
